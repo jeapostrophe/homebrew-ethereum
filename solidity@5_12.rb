1b2939f9144b609f989686625c00cd2299b83999
Jay McCarthy <jay.mccarthy@gmail.com>
2020-08-16 21:09:48 -0400

    Make a bunch of v5 formulas

---
+++ solidity@5_12.rb
@@ -0,0 +1,39 @@
+#------------------------------------------------------------------------------
+# solidity.rb
+#
+# Homebrew formula for solidity.  Homebrew (http://brew.sh/) is
+# the de-facto standard package manager for OS X, and this Ruby script
+# contains the metadata used to map command-line user settings used
+# with the 'brew' command onto build options.
+#
+# Our documentation for the solidity Homebrew setup is at:
+#
+# http://solidity.readthedocs.io/en/latest/installing-solidity.html
+#
+# (c) 2014-2017 solidity contributors.
+#------------------------------------------------------------------------------
+
+class SolidityAT512 < Formula
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "http://solidity.readthedocs.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.5.12/solidity_0.5.12.tar.gz"
+  version "0.5.12"
+  sha256 "cad9d5eaee79ce542f62c9c454f49b69bdb17d37f000029b425155e9c6816561"
+
+  keg_only "specific"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  # Note: due to a homebrew limitation, ccache will always be detected and cannot be turned off.
+  depends_on "ccache" => :build
+  depends_on "z3"
+
+  def install
+    system "cmake", ".", *std_cmake_args, "-DTESTS=OFF"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
---
+++ solidity@5_13.rb
@@ -0,0 +1,39 @@
+#------------------------------------------------------------------------------
+# solidity.rb
+#
+# Homebrew formula for solidity.  Homebrew (http://brew.sh/) is
+# the de-facto standard package manager for OS X, and this Ruby script
+# contains the metadata used to map command-line user settings used
+# with the 'brew' command onto build options.
+#
+# Our documentation for the solidity Homebrew setup is at:
+#
+# http://solidity.readthedocs.io/en/latest/installing-solidity.html
+#
+# (c) 2014-2017 solidity contributors.
+#------------------------------------------------------------------------------
+
+class SolidityAT513 < Formula
+  desc "The Solidity Contract-Oriented Programming Language"
+  homepage "http://solidity.readthedocs.org"
+  url "https://github.com/ethereum/solidity/releases/download/v0.5.13/solidity_0.5.13.tar.gz"
+  version "0.5.13"
+  sha256 "8d4b094b2144052c6aa275d9bf1bfea40b548a8f1925bb3690c4474dba73f544"
+
+  keg_only "specific"
+
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  # Note: due to a homebrew limitation, ccache will always be detected and cannot be turned off.
+  depends_on "ccache" => :build
+  depends_on "z3"
+
+  def install
+    system "cmake", ".", *std_cmake_args, "-DTESTS=OFF"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/solc", "--version"
+  end
+end
